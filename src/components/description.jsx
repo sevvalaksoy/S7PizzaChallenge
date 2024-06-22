01b5dcf084f5b23eed547afe8edd582b551c8624
sevvalaksoy <164859994+sevvalaksoy@users.noreply.github.com>
2024-06-22 10:47:29 +0300

    description

--- src/components/description.jsx
+++ src/components/description.jsx
@@ -7,9 +7,11 @@ const Name = styled.p`
     color: #292929;
 `
 const Cover = styled.section`
-    display: inline-block;
-    width: 574px;
-    text-align: left;
+    display: flex;
+    width: 100%;
+    background-color: #FAF7F2;
+    align-items: center;
+    flex-direction: column;
 `
 const Price = styled.div`
     font-family: "Barlow";
@@ -19,6 +21,7 @@ const Price = styled.div`
     font-size: 28px;
     align-items: center;
     color: #292929;
+    width: 100%;
 `
 const Descrp = styled.p`
     font-family: "Barlow";
@@ -26,22 +29,30 @@ const Descrp = styled.p`
     font-weight: 400;
     color: #5F5F5F;
     width: 580px;
+    text-align: left;
 `
+
 export default function Description (props) {
     const {price} = props;
 
     return (
         <Cover>
-            <Name>Position Absolute Acı Pizza</Name>
-            <Price>{price}₺
-                <Price style={{fontWeight:"400", fontSize:"16px", color:"#5F5F5F"}}>4.9 <div style={{width:"100px"}}></div>(200)</Price>
-            </Price>
-            <Descrp>Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. 
-                Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, 
-                daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, 
-                genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . 
-                Küçük bir pizzaya bazen pizzetta denir.
-            </Descrp>
+            <img src="../../Assets/mile2-aseets/pictures/form-banner.png"/>
+            <div>
+                <div style={{width:"607px", display:"flex", flexDirection:"column", alignItems:"flex-start"}}>
+                    <p style={{fontFamily:"Barlow", fontSize:"16px", fontWeight:"500", color:"#5F5F5F"}}>Ana Sayfa - <span style={{color:"#CE2829"}}>Sipariş Oluştur</span></p>
+                    <Name>Position Absolute Acı Pizza</Name>
+                    <Price>{price}₺
+                        <div style={{fontWeight:"400", fontSize:"16px", color:"#5F5F5F", display:"flex", justifyContent:"space-between", width:"30%"}}>4.9 <span>(200)</span></div>
+                    </Price>
+                    <Descrp>Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. 
+                        Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, 
+                        daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, 
+                        genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . 
+                        Küçük bir pizzaya bazen pizzetta denir.
+                    </Descrp>
+                </div>
+            </div>
         </Cover>
     )
 }
