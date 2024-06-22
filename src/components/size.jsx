c4d7217f87a1ae124ab0c29d4fca8b6f1a36ecad
sevvalaksoy <164859994+sevvalaksoy@users.noreply.github.com>
2024-06-22 12:46:12 +0300

    size1

--- src/components/size.jsx
+++ src/components/size.jsx
@@ -1,11 +1,21 @@
+import { hover } from "@testing-library/user-event/dist/cjs/convenience/hover.js";
+import { Button, ButtonGroup } from "reactstrap";
 import styled from "styled-components"
 
 const Cover = styled.section`
-    width: 580px;
+    width: 607px;
     display: flex;
     font-family: "Barlow";
     justify-content: space-between;
 `
+const buttonStyles = {
+    backgroundColor: '#FAF7F2',
+    width: '56px',
+    height: '56px',
+    borderRadius: '28px',
+    outline: 'none',
+    cursor: 'pointer', 
+  };
 
 export default function Size (props) {
 
@@ -19,24 +29,47 @@ export default function Size (props) {
         <Cover>
             <div style={{display:"flex", flexDirection:"column", alignItems:"flex-start", gap:"1rem", color:"#5F5F5F", marginTop:"1.5rem", fontWeight:"500"}}>
                 <p style={{fontFamily:"Barlow", fontSize:"20px", fontWeight:"600", color:"#292929", margin:"0"}}>Boyut Seç<i style={{color:"#D80027"}}>*</i></p>
-                <label >
-                    <input type="radio" name="boyut" value="Küçük" onChange={handleChange} checked={formData.boyut==="Küçük"} style={{marginRight:"1rem"}}/>Küçük
-                </label>
-                <label>
-                    <input type="radio" name="boyut" value="Orta" onChange={handleChange} data-cy="orta-boy" checked={formData.boyut==="Orta"} style={{marginRight:"1rem"}}/>Orta
-                </label>
-                <label>
-                    <input type="radio" name="boyut" value="Büyük" onChange={handleChange} checked={formData.boyut==="Büyük"} style={{marginRight:"1rem"}}/>Büyük
-                </label>
+                <ButtonGroup style={{display:"flex", gap:"1.5rem"}}>
+                    <Button
+                    name="boyut"
+                    value="Küçük"
+                    onClick={handleChange}
+                    checked={formData.boyut==="Küçük"}       
+                    className="buttons"
+                    style={buttonStyles}             >
+                    S
+                    </Button>
+                    <Button
+                    name="boyut"
+                    value="Orta" 
+                    data-cy="orta-boy"
+                    onClick={handleChange}
+                    checked={formData.boyut==="Orta"}   
+                    className="buttons"  
+                    style={buttonStyles}                >
+                    M
+                    </Button>
+                    <Button
+                    name="boyut"
+                    value="Büyük"
+                    onClick={handleChange}
+                    checked={formData.boyut==="Büyük"}   
+                    className="buttons"
+                    style={buttonStyles}                  >
+                    L
+                    </Button>
+                </ButtonGroup>
             </div>
-            <div style={{marginTop:"1.5rem", marginRight:"9.5rem"}}>
-                <p style={{fontFamily:"Barlow", fontSize:"20px", fontWeight:"600", color:"#292929", margin:"0", textAlign:"left"}}>Hamur Seç<i style={{color:"#D80027"}}>*</i></p>
-                <select data-cy="select-bar" value={formData.kalınlık} name="kalınlık" onChange={handleChange} style={{width:"150px", height:"20px", fontWeight:"bold"}}>
-                    <option value="inital">Hamur Kalınlığı</option>
-                    <option value="İnce">İnce Hamur</option>
-                    <option value="İncecik">İncecik Hamur</option>
-                    <option value="Klasik">Klasik Hamur</option>
+            <div style={{marginTop:"1.5rem"}}>
+                <p style={{fontFamily:"Barlow", fontSize:"20px", fontWeight:"600", color:"#292929", textAlign:"left", marginTop:"0"}}>Hamur Seç<i style={{color:"#D80027"}}>*</i></p>
+                <select data-cy="select-bar" value={formData.kalınlık} name="kalınlık" onChange={handleChange} 
+                style={{width:"283px", height:"56px", backgroundColor:"#FAF7F2", borderRadius:"6px", border:"0px", padding:"0 2rem", appearance:"none", cursor:"pointer"}}>
+                    <option value="inital" style={{fontWeight:"500",fontSize:"14px",color:"#5F5F5F", fontFamily:"Barlow"}}>-Hamur Kalınlığı Seç-</option>
+                    <option value="İnce" style={{fontWeight:"500",fontSize:"14px",color:"#5F5F5F", fontFamily:"Barlow",}}>İnce Hamur</option>
+                    <option value="İncecik" style={{fontWeight:"500",fontSize:"14px",color:"#5F5F5F", fontFamily:"Barlow",}}>İncecik Hamur</option>
+                    <option value="Klasik" style={{fontWeight:"500",fontSize:"14px",color:"#5F5F5F", fontFamily:"Barlow",}}>Klasik Hamur</option>
                 </select>
+                <img src="../../Assets/mile2-aseets/down-arrow.png" style={{width:"15px", height:"15px", marginLeft:"-2rem", marginBottom:"-0.2rem"}}/>
             </div>
         </Cover>
     )
